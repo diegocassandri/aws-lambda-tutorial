ec22e9596f6e7186ce9b035f37ca643ca4f14b94
diego.cassandri <diego.cassandri@senior.com.br>
2019-09-06 13:49:37 -0300

    first commit

---
+++ handler.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports.createChatMessage = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'createChatMessage',
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body
+    })
+  };
+  return response;
+};
+
+module.exports.getMessage = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'getMessage',
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body
+    })
+  };
+  return response;
+};
+
+module.exports.getRoomMessages = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'getRoomMessages',
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body
+    })
+  };
+  return response;
+};
+
+module.exports.getUserMessages = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'getUserMessages',
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body
+    })
+  };
+  return response;
+};
